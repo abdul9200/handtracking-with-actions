5a3cc7718c26086e87c3d571ad697943d5e00f5c
abdul9200 <72599767+abdul9200@users.noreply.github.com>
2022-04-28 23:13:15 +0000

    first commit

---
+++ code1.py
@@ -0,0 +1,135 @@
+import cv2
+import mediapipe as mp
+import pyautogui
+def countFingers(image, results, draw=True, display=True):
+    '''
+    This function will count the number of fingers up for each hand in the image.
+    Args:
+        image:   The image of the hands on which the fingers counting is required to be performed.
+        results: The output of the hands landmarks detection performed on the image of the hands.
+        draw:    A boolean value that is if set to true the function writes the total count of fingers of the hands on the
+                 output image.
+        display: A boolean value that is if set to true the function displays the resultant image and returns nothing.
+    Returns:
+        output_image:     A copy of the input image with the fingers count written, if it was specified.
+        fingers_statuses: A dictionary containing the status (i.e., open or close) of each finger of both hands.
+        count:            A dictionary containing the count of the fingers that are up, of both hands.
+    '''
+    
+    # Get the height and width of the input image.
+    height, width, _ = image.shape
+    
+    # Create a copy of the input image to write the count of fingers on.
+    output_image = image.copy()
+    
+    # Initialize a dictionary to store the count of fingers of both hands.
+    count = {'RIGHT': 0, 'LEFT': 0}
+    
+    # Store the indexes of the tips landmarks of each finger of a hand in a list.
+    fingers_tips_ids = [mp_hands.HandLandmark.INDEX_FINGER_TIP, mp_hands.HandLandmark.MIDDLE_FINGER_TIP,
+                        mp_hands.HandLandmark.RING_FINGER_TIP, mp_hands.HandLandmark.PINKY_TIP]
+    
+    # Initialize a dictionary to store the status (i.e., True for open and False for close) of each finger of both hands.
+    fingers_statuses = {'RIGHT_THUMB': False, 'RIGHT_INDEX': False, 'RIGHT_MIDDLE': False, 'RIGHT_RING': False,
+                        'RIGHT_PINKY': False, 'LEFT_THUMB': False, 'LEFT_INDEX': False, 'LEFT_MIDDLE': False,
+                        'LEFT_RING': False, 'LEFT_PINKY': False}
+    
+    
+    # Iterate over the found hands in the image.
+    for hand_index, hand_info in enumerate(results.multi_handedness):
+        
+        # Retrieve the label of the found hand.
+        hand_label = hand_info.classification[0].label
+        
+        # Retrieve the landmarks of the found hand.
+        hand_landmarks =  results.multi_hand_landmarks[hand_index]
+        
+        # Iterate over the indexes of the tips landmarks of each finger of the hand.
+        for tip_index in fingers_tips_ids:
+            
+            # Retrieve the label (i.e., index, middle, etc.) of the finger on which we are iterating upon.
+            finger_name = tip_index.name.split("_")[0]
+            
+            # Check if the finger is up by comparing the y-coordinates of the tip and pip landmarks.
+            if (hand_landmarks.landmark[tip_index].y < hand_landmarks.landmark[tip_index - 2].y):
+                
+                # Update the status of the finger in the dictionary to true.
+                fingers_statuses[hand_label.upper()+"_"+finger_name] = True
+                
+                # Increment the count of the fingers up of the hand by 1.
+                count[hand_label.upper()] += 1
+        
+        # Retrieve the y-coordinates of the tip and mcp landmarks of the thumb of the hand.
+        thumb_tip_x = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP].x
+        thumb_mcp_x = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP - 2].x
+        
+        # Check if the thumb is up by comparing the hand label and the x-coordinates of the retrieved landmarks.
+        if (hand_label=='Right' and (thumb_tip_x < thumb_mcp_x)) or (hand_label=='Left' and (thumb_tip_x > thumb_mcp_x)):
+            
+            # Update the status of the thumb in the dictionary to true.
+            fingers_statuses[hand_label.upper()+"_THUMB"] = True
+            
+            # Increment the count of the fingers up of the hand by 1.
+            count[hand_label.upper()] += 1
+     
+    # Check if the total count of the fingers of both hands are specified to be written on the output image.
+    if draw:
+
+        # Write the total count of the fingers of both hands on the output image.
+        cv2.putText(output_image, " Total Fingers: ", (10, 25),cv2.FONT_HERSHEY_COMPLEX, 1, (20,255,155), 2)
+        cv2.putText(output_image, str(sum(count.values())), (width//2-150,240), cv2.FONT_HERSHEY_SIMPLEX,
+                    8.9, (20,255,155), 10, 10)
+
+    # Check if the output image is specified to be displayed.
+    if display:
+        
+        # Display the output image.
+        plt.figure(figsize=[10,10])
+        plt.imshow(output_image[:,:,::-1]);plt.title("Output Image");plt.axis('off');
+    
+    # Otherwise
+    else:
+
+        # Return the output image, the status of each finger and the count of the fingers up of both hands.
+        return output_image, fingers_statuses, count
+mp_drawing = mp.solutions.drawing_utils
+
+mp_hands = mp.solutions.hands
+cap = cv2.VideoCapture(0)
+with mp_hands.Hands(
+    
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5) as hands:
+  while cap.isOpened():
+    success, image = cap.read()
+    if not success:
+      print("Ignoring empty camera frame.")
+      # If loading a video, use 'break' instead of 'continue'.
+      continue
+
+    # To improve performance, optionally mark the image as not writeable to
+    # pass by reference.
+    image.flags.writeable = False
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    results = hands.process(image)
+
+    # Draw the hand annotations on the image.
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    if results.multi_hand_landmarks:
+      for hand_landmarks in results.multi_hand_landmarks:
+        mp_drawing.draw_landmarks(
+            image,
+            hand_landmarks,
+            mp_hands.HAND_CONNECTIONS)
+        
+      image, fingers_statuses, count = countFingers(image, results, False,display=False)
+      if ((count['RIGHT']+count['LEFT'])%2==0):
+          pyautogui.press('A')
+      else:
+          pyautogui.press('B')
+    # Flip the image horizontally for a selfie-view display.
+    cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+      break
+cap.release()
